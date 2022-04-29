38dca663b2a2e315a47140d3f47a58603dc855cc
vic <vicjed@icloud.com>
2022-04-29 09:41:50 +0900

    feat(JDK_DYNAMIC_PROXY) : 동적 프록시 폼 사용

---
+++ src/test/java/hello/proxy/jdkDynamic/JDKDynamicProxyTest.java
@@ -0,0 +1,31 @@
+package hello.proxy.jdkDynamic;
+
+import hello.proxy.jdkDynamic.code.AImpl;
+import hello.proxy.jdkDynamic.code.AInterface;
+import hello.proxy.jdkDynamic.code.TimeInvocationHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Proxy;
+
+@Slf4j
+public class JDKDynamicProxyTest {
+
+    @Test
+    void dynamicA() {
+        AInterface target = new AImpl();
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+        Object o = Proxy.newProxyInstance(AInterface.class.getClassLoader(),
+                new Class[]{AInterface.class}, handler);
+
+        AInterface aInterface = (AInterface) o;
+
+        aInterface.call();
+
+        log.info("targetClass= {}", target.getClass());
+        log.info("proxyClass= {}", aInterface.getClass());
+
+    }
+
+}
